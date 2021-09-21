417fe485ea9ee6ef6955739699d7585bc1f9193b
404b0tnet <jsmith728.js@gmail.com>
2021-09-21 11:23:41 -0400

    init

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+#![allow(non_snake_case)]
+fn main() {
+ let mut line = String::new();
+ println!( "Please enter your name: ");
+ std::io::stdin().read_line(&mut line).unwrap();
+ println!("{}", build_message(line));
+}
+
+fn build_message(name: String) -> String {
+    let message = "You are welcome ".to_owned() + &name;
+    return message;
+}
+
+#[test]
+fn test_build_message(){
+    let name = String::from("Jack");
+
+    assert_eq!(build_message(name), "You are welcome Jack")
+}
